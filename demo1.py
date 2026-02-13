8c5f23486a591d097032ab37b9881af01ffa09ae
ketangoyal2004-data <ketangoyal858@gmail.com>
2026-02-13 08:55:11 +0530

    This is a new files

---
+++ demo1.py
@@ -0,0 +1,10 @@
+class cal:
+    def add(self,a,b):
+        return a + b
+
+    def sub(self,a,b):
+        return a - b
+    
+obj = cal()
+print(obj.add(10,20))
+print(obj.sub(10,20))
---
+++ demo2.py
@@ -0,0 +1,10 @@
+class std:
+    def name(self):
+        return "Harry"
+    
+    def name2(self):
+        return "Darry"
+    
+obj = std()
+print(obj.name())   
+print(obj.name2()) 
